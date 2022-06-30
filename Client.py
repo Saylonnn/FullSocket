c2b51d89e423f49227581ca41818ac965a9d2658
Saylonnn <85989616+Saylonnn@users.noreply.github.com>
2022-06-30 22:36:34 +0200

    Added Client, clientThread, threadedServer

--- Client.py
+++ Client.py
@@ -1,9 +1,11 @@
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((str(socket.gethostbyname(socket.gethostname())), 8485))
 client_socket.connect(("192.168.117.230", 8485))
+print("connected")
 while True:
     data = client_socket.recv(1024)
     data = data.decode('utf-8')
-    print(data)
+    print("data: ", data)
 
--- threadedServer.py
+++ threadedServer.py
@@ -1,29 +1,43 @@
 import socket
 import threading
-from clientThread import ClientThread
+
 
 class ThreadedServer():
     def __init__(self, host, port):
         self.host = host
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR)
+        print("Socket created")
+
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.host, self.port))
+        print("Socket binded")
         self.connections = []
 
     def listen(self, maxConnectedPorts=5):
         self.sock.listen(maxConnectedPorts)
         while True:
             client, address = self.sock.accept()
-            #client.settimeout(60)
-            conn = ClientThread(client, address)
-            conn.start()
-            self.connections.append(conn)
+            print("Accepted ", address)
+            self.connections.append(client)
+            threading.Thread(target = self.listenToClient, args = (client,address)).start()
 
-    def sendToAllString(self, data):
+    def listenToClient(self, client, address):
+        size = 1024
+        while True:
+            try:
+                data = client.recv(size)
+                if data:
+                    print(data)
+
+            except ConnectionError:
+                client.close()
+                return False
+
+    def send_to_all_string(self, data):
         for x in self.connections:
             try:
                 x.sendData(data)
             except ConnectionError:
-                print(x , " has disconnected.")
+                print(x, " has disconnected.")
                 self.connections.remove(x)
